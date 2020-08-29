75f0162f709759b2b4c6ffb83149ee5e382ea8d0
rchia013 <rchia013@e.ntu.edu.sg>
2020-08-30 02:34:35 +0800

    30/8 0234

---
+++ Assets/SizeBoost.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SizeBoost : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float multiplier = 1.4f;
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Pickup(other);
+        }
+    }
+
+    
+    void Pickup(Collider player)
+    {
+        player.transform.localScale *= multiplier;
+        Destroy(gameObject);
+
+    }
+}
