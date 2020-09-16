fab79ea7c50a02a9d4162503022eff635160060c
rchia013 <60123513+rchia013@users.noreply.github.com>
2020-09-16 21:14:35 +0800

    16 Sept 914pm

--- Assets/HighscoreTable.cs
+++ Assets/HighscoreTable.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HighscoreTable : MonoBehaviour
 {
     private Transform entryContainer;
     private Transform entryTemplate;
     float templateHeight = 20f;
+    string rankString;
 
     private void Awake()
     {
@@ -19,8 +21,27 @@ public class HighscoreTable : MonoBehaviour
         {
             Transform entryTransform = Instantiate(entryTemplate, entryContainer);
             RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
-            entryRectTransform.anchoredPosition = new Vector2(0, -templateHeight * i);
+            entryRectTransform.anchoredPosition = new Vector3(50, -templateHeight * i - 20,0);
             entryTransform.gameObject.SetActive(true);
+
+            int rank = i + 1;
+            switch (rank)
+            {
+                default:
+                    rankString = rank + "th";break;
+                case 1:
+                    rankString = "1st"; break;
+                case 2:
+                    rankString = "2nd"; break;
+                case 3:
+                    rankString = "3rd"; break;
+            }
+
+            entryTransform.Find("Rank").GetComponent<Text>().text = rankString;
+            int score = Random.Range(0, 100);
+            entryTransform.Find("Points").GetComponent<Text>().text = score.ToString();
+            string playerName = "Player " + (i+1).ToString();
+            entryTransform.Find("Name").GetComponent<Text>().text = playerName;
         }
     }
 }
