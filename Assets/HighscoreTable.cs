b325c14e0d2d2a2139db2354bb9e08699a750e7f
rchia013 <60123513+rchia013@users.noreply.github.com>
2020-09-16 18:10:33 +0800

    16 sept 1810

---
+++ Assets/HighscoreTable.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighscoreTable : MonoBehaviour
+{
+    private Transform entryContainer;
+    private Transform entryTemplate;
+    float templateHeight = 20f;
+
+    private void Awake()
+    {
+        entryContainer = transform.Find("HighScoreEntryContainer");
+        entryTemplate = entryContainer.Find("HighScoreEntryTemplate");
+
+        entryTemplate.gameObject.SetActive(false);
+
+        for (int i = 0; i < 4; i++)
+        {
+            Transform entryTransform = Instantiate(entryTemplate, entryContainer);
+            RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
+            entryRectTransform.anchoredPosition = new Vector2(0, -templateHeight * i);
+            entryTransform.gameObject.SetActive(true);
+        }
+    }
+}
