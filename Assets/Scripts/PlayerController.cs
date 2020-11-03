4a8726e15e218ad2d9e80d0df423f9a93b8b3015
chavalitpramotedham <chavalit@pramotedham.com>
2020-11-04 00:21:32 +0800

    Chava 4 Nov 12.21am

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -152,6 +152,8 @@ public class PlayerController : MonoBehaviourPunCallbacks
 
         if (direction.magnitude >= 0.1f)
         {
+            PV.RPC("setRootMotion", RpcTarget.All, false);
+
             //anim.enabled = true;
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
@@ -163,6 +165,8 @@ public class PlayerController : MonoBehaviourPunCallbacks
             {
                 PV.RPC("setWalking", RpcTarget.All, true);
             }
+
+            PV.RPC("setRootMotion", RpcTarget.All, true);
         }
         else
         {
