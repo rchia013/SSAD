9626e1b2a1dea0d42f80f90ea91020a3c9f30434
lisasgoh <42923450+lisasgoh@users.noreply.github.com>
2020-11-03 17:42:26 +0800

    Lisa 3 Nov 542 PM
    Username always faces camera

---
+++ Assets/Scripts/Username.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Username : MonoBehaviour
+{
+    public Camera cameraToLookAt;
+    void Start()
+    {
+        cameraToLookAt = GameSetUp.GS.playerCam.GetComponent<Camera>();
+    }
+    void Update()
+    {
+        Vector3 v = cameraToLookAt.transform.position - transform.position;
+        v.x = v.z = 0.0f;
+        transform.LookAt(cameraToLookAt.transform.position - v);
+        transform.Rotate(0, 180, 0);
+    }
+}
