f95bc6a5c03cae449f2e4e9c5977c05ccdf76ac3
rchia013 <rchia013@e.ntu.edu.sg>
2020-11-11 17:15:20 +0800

    Reuben 11/11 515pm

--- Assets/Scripts/Controllers/MainMenu/Achievement_controller.cs
+++ Assets/Scripts/Controllers/MainMenu/Achievement_controller.cs
@@ -7,15 +7,21 @@ using Proyecto26;
 using Newtonsoft.Json;
 using UnityEngine.UI;
 
+/// <summary>
+/// This class handles the logic behind the achievements of each user
+/// </summary>
 public class Achievement_controller : MonoBehaviour
 {
+    //List that stores the users information
     public List<Achievement> playerinfo = new List<Achievement>();
+
+    //The Badges that the players will be able to get
     public GameObject Bronze;
     public GameObject Silver;
     public GameObject Gold;
-
     public GameObject firstBar;
 
+    //Text that displays the user's points and username
     public TextMeshProUGUI points;
     public TextMeshProUGUI nameBox;
     string localID = Login.localid;
@@ -23,15 +29,19 @@ public class Achievement_controller : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
         Achievement playerinfo = new Achievement();
         string playerurl = "https://quizguyz.firebaseio.com/Users/" + localID;
+
+        //API Call to database to retrieve user's information
         RestClient.Get(url: playerurl + ".json").Then(onResolved: response =>
         {
             playerinfo = JsonConvert.DeserializeObject<Achievement>(response.Text);
-
             points.text = playerinfo.achievementPoints.ToString();
             nameBox.text = playerinfo.username;
 
+            //Handles the badges that the player has
+            //E.g. If more than 750 achievement points, player will have the Gold badge
             if (playerinfo.achievementPoints <= 750)
             {
                 firstBar.GetComponent<Image>().fillAmount = ((float)playerinfo.achievementPoints / 750);
@@ -41,7 +51,7 @@ public class Achievement_controller : MonoBehaviour
                 firstBar.GetComponent<Image>().fillAmount = 1;
             }
             
-
+            //Displays the badges that the player has on the UI
             if (playerinfo.achievementPoints >= 250)
             {
                 Bronze.SetActive(true);
