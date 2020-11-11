e4505c8e465365fc4a224d936e95d2a6993263de
rchia013 <rchia013@e.ntu.edu.sg>
2020-11-11 18:42:52 +0800

    Reuben 11/11 642pm

--- Assets/Scripts/Gameplay/UI/PointsListInitilialize.cs
+++ Assets/Scripts/Gameplay/UI/PointsListInitilialize.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// This class handles the points UI during the gameplay.
+/// </summary>
 public class PointsListInitilialize : MonoBehaviour
 {
-
+    // UI of the points dispay
     public GameObject[] borders;
     public GameObject[] playerBoxes;
-
     public Material blue;
     public Material pink;
     public Material green;
@@ -38,13 +40,18 @@ public class PointsListInitilialize : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Sets the color of up box based on the colour of the player.
+    /// </summary>
+    /// <param name="index">The index.</param>
+    /// <param name="choice">The choice.</param>
     void setUpBoxColor(int index, int choice)
     {
         playerBoxes[index].SetActive(true);
-
         int color = choice % 10;
         Color chosen = Color.white;
 
+        // Sets up the colour of the points box based on the user's colour.
         switch (color)
         {
             case 1:
@@ -73,10 +80,12 @@ public class PointsListInitilialize : MonoBehaviour
         }
 
         playerBoxes[index].GetComponent<Image>().color = chosen;
-
-
     }
 
+    /// <summary>
+    /// Highlights the box of each user based on their colour.
+    /// </summary>
+    /// <param name="index">The index.</param>
     void setUpHighlight(int index)
     {
         borders[index].SetActive(true);
