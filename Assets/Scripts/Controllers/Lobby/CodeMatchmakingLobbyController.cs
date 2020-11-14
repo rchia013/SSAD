6fb12fa656b982ef6913a2d71d743e0a046d0750
lisasgoh <42923450+lisasgoh@users.noreply.github.com>
2020-11-14 21:00:38 +0800

    9PM 14 Nov
    Add comments

--- Assets/Scripts/Controllers/Lobby/CodeMatchmakingLobbyController.cs
+++ Assets/Scripts/Controllers/Lobby/CodeMatchmakingLobbyController.cs
@@ -77,6 +77,9 @@ public class CodeMatchmakingLobbyController : MonoBehaviourPunCallbacks
         }
     }
 
+    /// <summary>
+    /// Initialise the Difficulty and Category buttons in the Create/Join Room screen
+    /// </summary>
     private void InitializeButtons()
     {
         buttonsCat.Add(math);
@@ -102,6 +105,10 @@ public class CodeMatchmakingLobbyController : MonoBehaviourPunCallbacks
         }
     }
 
+    /// <summary>
+    /// When a category is selected, 
+    /// </summary>
+    /// <param name="index"></param>
     void CatClicked(int index)
     {
         if (!catChosen)
@@ -132,6 +139,11 @@ public class CodeMatchmakingLobbyController : MonoBehaviourPunCallbacks
         LobbySetUp.LS.category = cat;
     }
 
+    /// <summary>
+    /// When a difficuly is selected, the other difficulty levels cannot be selected.
+    /// To select another difficulty level, you must click on the current one to unselect it first.
+    /// </summary>
+    /// <param name="index"></param>
     void DiffClicked(int index)
     {
         if (!diffChosen)
@@ -163,9 +175,13 @@ public class CodeMatchmakingLobbyController : MonoBehaviourPunCallbacks
     }
 
 
-
+    /// <summary>
+    /// This is a callback function provided in the MonoBehaviourPunCallbacks class  provided by PUN 2 
+    /// Called when the client is connected to the Master Server and ready for matchmaking and other tasks
+    /// </summary>
     public override void OnConnectedToMaster()
     {
+        // This defines if all clients in a room should automatically load the same level as the Master Client.
         PhotonNetwork.AutomaticallySyncScene = true;
 
         MainPanel.SetActive(false);
@@ -177,6 +193,9 @@ public class CodeMatchmakingLobbyController : MonoBehaviourPunCallbacks
         PhotonNetwork.NickName = Login.currentUser.username;
     }
 
+    /// <summary>
+    /// Join lobby when clicked. Set the panels accordingly.
+    /// </summary>
     public void JoinLobbyOnClick()
     {
         MainPanel.SetActive(false);
@@ -184,13 +203,19 @@ public class CodeMatchmakingLobbyController : MonoBehaviourPunCallbacks
         PhotonNetwork.JoinLobby();
     }
 
-    // CREATOR:
-
+    /// <summary>
+    /// Detects when the room size input value changes and updates accordingly
+    /// </summary>
+    /// <param name="sizeIn"></param>
     public void OnRoomSizeInputChanged(string sizeIn)
     {
         roomSize = int.Parse(sizeIn);
     }
 
+    /// <summary>
+    /// Create a room on click. It generates a 4 digit room code automatically and creates the room with the specified room options.
+    /// The room options sets whether the room is visible in the lobby, is open to be joined, and the maximum number of players.
+    /// </summary>
     public void CreateRoomOnClick()
     {
         RoomOptions roomOps = new RoomOptions() { IsVisible = true, IsOpen = true, MaxPlayers = (byte)roomSize };
@@ -211,6 +236,13 @@ public class CodeMatchmakingLobbyController : MonoBehaviourPunCallbacks
         codeDisplay.text = "Code: " + roomName;
     }
 
+    /// <summary>
+    /// This is a callback function provided in the MonoBehaviourPunCallbacks class  provided by PUN 2 
+    /// A callback for when the room creation failed, which can be due to an existing room with the same room code
+    /// This prompts the creation of a room again.
+    /// </summary>
+    /// <param name="returnCode"></param>
+    /// <param name="message"></param>
     public override void OnCreateRoomFailed(short returnCode, string message)
     {
         RoomOptions roomOps = new RoomOptions() { IsVisible = true, IsOpen = true, MaxPlayers = (byte)roomSize };
@@ -223,8 +255,12 @@ public class CodeMatchmakingLobbyController : MonoBehaviourPunCallbacks
         codeDisplay.text = roomName;
     }
 
+    /// <summary>
+    /// Cancels the room when clicked.
+    /// </summary>
     public void CancelRoomOnClick()
     {
+        // This requests each player client to disconnect if the requesting user is the master client.
         if (PhotonNetwork.IsMasterClient)
         {
             foreach (Player player in PhotonNetwork.PlayerList)
@@ -232,40 +268,43 @@ public class CodeMatchmakingLobbyController : MonoBehaviourPunCallbacks
                 PhotonNetwork.CloseConnection(player);
             }
         }
+        // Leave the current room and return to the Master Server where you can join or create rooms
         PhotonNetwork.LeaveRoom();
         RoomPanel.SetActive(false);
     }
 
-    // JOINERS:
-
+    // This is a function that is called when the code player by the input changes and updates the variable.
     public void OnCodeInputChanged(string code)
     {
         joinCode = code;
     }
 
+    /// <summary>
+    /// Joins the room when clicked, with a joinCode
+    /// </summary>
     public void JoinRoomOnClick()
     {
         PhotonNetwork.JoinRoom(joinCode);
         codeDisplay.text = "Code: "+joinCode;
-
-        //if (!roomController.GetComponent<AvatarController>().isCreator)
-        //{
-        //    roomController.GetComponent<AvatarController>().isCreator = false;
-        //}
-        // roomController.GetComponent<AvatarController>().enabled = true;
-
-        // lobbyPanel.SetActive(false);
-        // RoomPanel.SetActive(true);
     }
 
+    /// <summary>
+    /// Leaves the room when clicked
+    /// </summary>
     public void LeaveRoomOnClick()
     {
+        // Checks whether the player is in the room, if so,
+        // Leave the current room and return to the Master Server where you can join or create rooms
         if (PhotonNetwork.InRoom)
         {
             PhotonNetwork.LeaveRoom();
         }
     }
 
+    /// <summary>
+    /// This is a callback function provided in the MonoBehaviourPunCallbacks class provided by PUN 2 
+    /// Called when the local user/client left a room, so the game's logic can clean up it's internal state.
+    /// </summary>
     public override void OnLeftRoom()
     {
         RoomPanel.SetActive(false);
--- Assets/Scripts/Controllers/RoomAndSetUp/CodeMatchmakingRoomController.cs
+++ Assets/Scripts/Controllers/RoomAndSetUp/CodeMatchmakingRoomController.cs
@@ -6,10 +6,16 @@ using TMPro;
 using UnityEngine.SceneManagement;
 using System.Collections.Generic;
 
+/// <summary>
+/// This script controls the logic of the 
+/// </summary>
 public class CodeMatchmakingRoomController : MonoBehaviourPunCallbacks
 {
+    // Player count display
     [SerializeField]
     private TextMeshProUGUI playerCount;
+
+    // Multiplayer scene index
     [SerializeField]
     private int multiplayerSceneIndex;
 
@@ -29,11 +35,17 @@ public class CodeMatchmakingRoomController : MonoBehaviourPunCallbacks
     [SerializeField]
     private Button mapButton;
 
+    /// <summary>
+    /// Update is called every frame, and is the main function used to determine if the host can start the game
+    /// </summary>
     public void Update()
     {
         startButton.interactable = readyToStart();
     }
 
+    /// <summary>
+    /// This is called to ensure that all the players in the Room have chosen an avatar, so that the host is able to start.
+    /// </summary>
     private bool readyToStart()
     {
         if (LobbySetUp.LS.playerList.Count == PhotonNetwork.PlayerList.Length)
@@ -57,6 +69,10 @@ public class CodeMatchmakingRoomController : MonoBehaviourPunCallbacks
         return true;
     }
 
+    /// <summary>
+    /// This is a callback function provided in the MonoBehaviourPunCallbacks class provided by PUN 2
+    /// Called when the LoadBalancingClient entered a room, no matter if this client created it or simply joined.
+    /// </summary>
     public override void OnJoinedRoom()
     {
 
@@ -95,21 +111,38 @@ public class CodeMatchmakingRoomController : MonoBehaviourPunCallbacks
         }
     }
 
+    /// <summary>
+    /// This is a callback function provided in the MonoBehaviourPunCallbacks class provided by PUN 2
+    /// Called when a remote player entered the room. 
+    /// This Player is already added to the playerlist.
+    /// </summary>
     public override void OnPlayerEnteredRoom(Player newPlayer)
     {
+        // The player count is updated for each client
         playerCount.text = PhotonNetwork.PlayerList.Length + " Players";
 
+        // This calls the addPlayer() function in the AvatarController class
         gameObject.GetComponent<AvatarController>().addPlayer(newPlayer.NickName, true);
     }
-    
+
+    /// <summary>
+    /// This is a callback function provided in the MonoBehaviourPunCallbacks class provided by PUN 
+    /// Called when a remote player left the room or became inactive. 
+    /// </summary>
     public override void OnPlayerLeftRoom(Player otherPlayer)
     {
+        // The player count is updated for each client
         playerCount.text = PhotonNetwork.PlayerList.Length + " Players";
 
+        // This calls the removePlayer() function in the AvatarController class
         gameObject.GetComponent<AvatarController>().removePlayer(otherPlayer.NickName);
     }
 
-    
+    /// <summary>
+    /// This is a callback function provided in the MonoBehaviourPunCallbacks class  provided by PUN 2 
+    /// Called when the local user/client left a room, so the game's logic can clean up it's internal state.
+    /// When leaving a room, the LoadBalancingClient will disconnect the Game Server and connect to the Master Server.
+    /// </summary>
     public override void OnLeftRoom()
     {
         gameObject.SetActive(false);
@@ -121,12 +154,21 @@ public class CodeMatchmakingRoomController : MonoBehaviourPunCallbacks
         PhotonNetwork.Disconnect();
         SceneManager.LoadScene("CodeMatchMakingMenuDemo");
     }
-
+    /// <summary>
+    /// This is a callback function provided in the MonoBehaviourPunCallbacks class  provided by PUN 2 
+    /// Called when a previous OpJoinRoom call failed on the server
+    /// </summary>
+    /// <param name="returnCode"></param>
+    /// <param name="message"></param>
     public override void OnJoinRoomFailed(short returnCode, string message)
     {
         Debug.Log(message);
     }
 
+    /// <summary>
+    /// This function is triggered when the host clicks "Start". It checks that all the players in the Room
+    /// have all chosen an avatar, before it loads the game.
+    /// </summary>
     public void StartGameOnClick()
     {
         for (int i = 0; i < LobbySetUp.LS.playerList.Count; i++)
